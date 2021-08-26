d45ee71a3df8e1b6adc3866bd6b6f238f2f52f93
GCampbell2001 <GCampbell@student.neumont.edu>
2021-08-26 15:11:15 -0600

    started the table generating

--- degree.js
+++ degree.js
@@ -1,5 +1,13 @@
-
+let courseList = [1, 2, 3, 4, 5];
 
 function signout() {
     location.replace("index.html");
+}
+
+function generateTable() {
+    var table = document.getElementById("courseTable");
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+
+    
 }
