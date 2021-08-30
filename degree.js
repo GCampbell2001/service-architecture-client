123a45768570feda1a91961e2d9e19c3ee306b34
GCampbell2001 <GCampbell@student.neumont.edu>
2021-08-29 20:23:55 -0600

    Setting up Requests to populate User Info

--- degree.js
+++ degree.js
@@ -1,4 +1,4 @@
-let courseList = ["11"];
+let courseList = ["15"];
 let courseInfo = [];
 let headerList = ["Code", "Course Name", "Course Time", "Start Date", "End Date", "Instructor"];
 
@@ -7,8 +7,23 @@ function signout() {
 }
 
 window.onload = function loadCatalog() {
+    LoadTable();
     TableGenerate();
 
+}
+
+function LoadTable() {
+    var username = sessionStorage.getItem("username");
+    var password = sessionStorage.getItem("password");
+
+     fetch('http://localhost:9090/users/' + username + '/' + password).then(res =>
+     res.json()).then(data => {list = data;
+        document.getElementById("idUser").innerHTML(data.id);
+        document.getElementById("nameUser").innerHTML(data.name);
+        document.getElementById("Email").innerHTML(data.email);
+        document.getElementById("birthdayUser").innerHTML(data.birthday);
+        document.getElementById("degreeUser").innerHTML(data.degree);
+     });
 }
 
 function TableGenerate() {
@@ -53,6 +68,19 @@ function populateCourseInfo(CourseCode){
     //     courseInfo.push(data.endDate);
     // });
 
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("POST", "http://localhost:9090/courses");
+    xmlHttp.setRequestHeader('Access-Control-Allow-Headers', '*');
+    // xmlHttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+    xmlHttp.setRequestHeader('Content-Type', 'application/json');
+    xmlHttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xmlHttp.send();
+    xmlHttp.onreadystatechange = function(){
+        if(xmlHttp.status == 200){
+            var json_data = xmlHttp.responseText;
+            console.log(json_data);
+            // return json_data;
+        }
+    }
 
-    
 }
--- index.js
+++ index.js
@@ -29,3 +29,28 @@ function cancelButton() {
     document.getElementById("logo").style.display = 'block';
     document.getElementById('id01').style.display='none';
 }
+
+function login() {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("POST", "http://localhost:9090/users/" + username + "/" + password);
+    xmlHttp.setRequestHeader('Access-Control-Allow-Headers', '*');
+    // xmlHttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+    xmlHttp.setRequestHeader('Content-Type', 'application/json');
+    xmlHttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xmlHttp.send();
+    xmlHttp.onreadystatechange = function(){
+        if(xmlHttp.status == 200){
+            var json_data = xmlHttp.responseText;
+            sessionStorage.setItem("user", json_data);
+            if(json_data.includes("STUDENT")){
+                window.location.href("./degree.html");
+            } else if(json_data.includes("TEACHER")){
+                window.location.href("./tdegree.html");
+            }
+            // return json_data;
+        }
+    }
+    // window.location.href("http://127.0.0.1:5500/degree.html");
+    // window.open("degree.html", '_self');
+    // document.location.href = "degree.html";
+}
